362d8ec9c919304f37778917c6c464bf1bea3445
MathieuPinger <mathieu.pinger@zi-mannheim.de>
2020-11-25 16:25:39 +0100

    multiple trials (timeline)

--- jspsych_experiments.js
+++ jspsych_experiments.js
@@ -1,63 +1,66 @@
 // create trial data
-// trialData = [{
-//     "id": 1,
-//     "immediate": 1,
-//     "delayed": 2,
-//     "delay": "2 Wochen"
-// },
-// {
-//     "id": 2,
-//     "immediate": 1.50,
-//     "delayed": 2.50,
-//     "delay": "4 Wochen"
-// },{
-//     "id": 3,
-//     "immediate": 1,
-//     "delayed": 4,
-//     "delay": "1 Jahr"
-// },{
-//     "id": 4,
-//     "immediate": 5,
-//     "delayed": 10,
-//     "delay": "1 Woche"
-// },{
-//     "id": 5,
-//     "immediate": 2,
-//     "delayed": 2.50,
-//     "delay": "2 Wochen"
-// }];
+trialList = [{
+    "id": 1,
+    "immediate": 1,
+    "delayed": 2,
+    "delay": "2 Wochen"
+},
+{
+    "id": 2,
+    "immediate": 1.50,
+    "delayed": 2.50,
+    "delay": "4 Wochen"
+},{
+    "id": 3,
+    "immediate": 1,
+    "delayed": 4,
+    "delay": "1 Jahr"
+},{
+    "id": 4,
+    "immediate": 5,
+    "delayed": 10,
+    "delay": "1 Woche"
+},{
+    "id": 5,
+    "immediate": 2,
+    "delayed": 2.50,
+    "delay": "2 Wochen"
+}];
 
-// console.log(trialData);
+// initialize timeline
+const timeline = [];
 
-let timeline = [];
+// add trials to timeline: loop through trialList
+trialList.map(trial => {
+    let trialData = {
+        type: "html-keyboard-response",
+        stimulus:
+        `<div class = centerbox id='container'>
+        <p class = center-block-text>
+            Please select the option that you would prefer pressing
+            <strong>'q'</strong> for left
+            <strong>'p'</strong> for right:
+        </p>
+        <div class='table'>
+        <div class='row'>
+        <div id = 'option'><center><font color='green'>
+            ${trial.immediate}
+        <br>
+            Heute
+        </font></center></div>
+        <div id = 'option'><center><font color='green'>
+            ${trial.delayed}
+        <br>
+            in ${trial.delay}
+        </font></center></div></div></div></div>`,
+        choices: ['q', 'p'],
+        stimulus_duration: 2000,
+        trial_duration: 2000
+    }
+    timeline.push(trialData)
+});
 
-let trial = {
-    type: "html-keyboard-response",
-    stimulus:
-    `<div class = centerbox id='container'>
-    <p class = center-block-text>
-        Please select the option that you would prefer pressing
-        <strong>'q'</strong> for left
-        <strong>'p'</strong> for right:
-    </p>
-    <div class='table'>
-    <div class='row'>
-    <div id = 'option'><center><font color='green'>
-        1€
-    <br>
-        now
-    </font></center></div>
-    <div id = 'option'><center><font color='green'>
-        1€
-    <br>
-        in 2 weeks
-    </font></center></div></div></div></div>`,
-    choices: ['q', 'p'],
-    stimulus_duration: 2000,
-    trial_duration: 2000
-}
 
-timeline.push(trial)
 
 /* needed:
 post_trial_gap
